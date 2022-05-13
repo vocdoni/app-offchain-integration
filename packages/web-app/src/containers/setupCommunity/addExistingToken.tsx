72ed25285d1c4e3cb371e310981e60f64fd28dcb
Fabrice Francois <fabricevladimir@gmail.com>
2022-05-13 05:32:03 -0400

    Feature: Fetch existing token on erc20 Voting DAO creation (#352)
    * added method to get custom provider
    
    * fixed uncontrolled field warning
    
    * review changes

--- packages/web-app/src/containers/setupCommunity/addExistingToken.tsx
+++ packages/web-app/src/containers/setupCommunity/addExistingToken.tsx
@@ -12,19 +12,15 @@ import styled from 'styled-components';
 import {chains} from 'use-wallet';
 import {ChainInformation} from 'use-wallet/dist/cjs/types';
 
-import {useProviders} from 'context/providers';
-import {useNetwork} from 'context/network';
+import {useSpecificProvider} from 'context/providers';
 import {formatUnits} from 'utils/library';
 import {getTokenInfo} from 'utils/tokens';
 import {validateTokenAddress} from 'utils/validators';
-import {CHAIN_METADATA} from 'utils/constants';
 
 const DEFAULT_BLOCK_EXPLORER = 'https://etherscan.io/';
 
 const AddExistingToken: React.FC = () => {
   const {t} = useTranslation();
-  const {network} = useNetwork();
-  const {infura: provider} = useProviders();
   const {control, setValue, trigger} = useFormContext();
 
   const [tokenAddress, blockchain, tokenName, tokenSymbol, tokenTotalSupply] =
@@ -38,7 +34,7 @@ const AddExistingToken: React.FC = () => {
       ],
     });
 
-  const chainId = CHAIN_METADATA[network].id;
+  const provider = useSpecificProvider(blockchain.id);
   const explorer = useMemo(() => {
     if (blockchain.id) {
       // TODO move necessary information from useWallet's ChainInformation into
@@ -54,10 +50,10 @@ const AddExistingToken: React.FC = () => {
 
   // Trigger address validation on network change
   useEffect(() => {
-    if (blockchain.id && tokenAddress !== '' && !tokenSymbol) {
+    if (blockchain.id && tokenAddress !== '') {
       trigger('tokenAddress');
     }
-  }, [blockchain.id, chainId, tokenAddress, tokenSymbol, trigger]);
+  }, [blockchain.id, tokenAddress, trigger]);
 
   /*************************************************
    *            Functions and Callbacks            *
@@ -131,20 +127,21 @@ const AddExistingToken: React.FC = () => {
             </>
           )}
         />
-        {tokenName && (
+        {tokenName && tokenAddress && (
           <TokenInfoContainer>
             <InfoContainer>
               <Label label={t('labels.tokenName')} />
-              <TextInput disabled value={tokenName} />
+              <TextInput disabled value={tokenName || ''} />
             </InfoContainer>
             <InfoContainer>
               <Label label={t('labels.tokenSymbol')} />
-              <TextInput disabled value={tokenSymbol} />
+              <TextInput disabled value={tokenSymbol || ''} />
             </InfoContainer>
             <InfoContainer>
               <Label label={t('labels.supply')} />
               <TextInput
                 disabled
+                defaultValue=""
                 value={new Intl.NumberFormat('en-US', {
                   maximumFractionDigits: 4,
                 }).format(tokenTotalSupply)}
--- packages/web-app/src/context/providers.tsx
+++ packages/web-app/src/context/providers.tsx
@@ -2,7 +2,7 @@ import {InfuraProvider, Web3Provider} from '@ethersproject/providers';
 import React, {createContext, useContext, useEffect, useState} from 'react';
 import {useWallet} from 'use-wallet';
 
-import {INFURA_PROJECT_ID_ARB} from 'utils/constants';
+import {INFURA_PROJECT_ID_ARB, SupportedChainID} from 'utils/constants';
 import {Nullable} from 'utils/types';
 
 const NW_ARB = {chainId: 42161, name: 'arbitrum'};
@@ -41,21 +41,7 @@ export function ProvidersProvider({children}: ProviderProviderProps) {
   );
 
   useEffect(() => {
-    // NOTE Passing the chainIds from useWallet doesn't work in the case of
-    // arbitrum and arbitrum-rinkeby. They need to be passed as objects.
-    // However, I have no idea why this is necessary. Looking at the ethers
-    // library, there's no reason why passing the chainId wouldn't work. Also,
-    // I've tried it on a fresh project and had no problems there...
-    // [VR 07-03-2022]
-    if (chainId === 42161) {
-      setInfuraProvider(new InfuraProvider(NW_ARB, INFURA_PROJECT_ID_ARB));
-    } else if (chainId === 421611) {
-      setInfuraProvider(
-        new InfuraProvider(NW_ARB_RINKEBY, INFURA_PROJECT_ID_ARB)
-      );
-    } else {
-      setInfuraProvider(new InfuraProvider(chainId, INFURA_PROJECT_ID_ARB));
-    }
+    setInfuraProvider(getInfuraProvider(chainId as SupportedChainID));
   }, [chainId]);
 
   useEffect(() => {
@@ -69,6 +55,39 @@ export function ProvidersProvider({children}: ProviderProviderProps) {
       {children}
     </ProviderContext.Provider>
   );
+}
+
+function getInfuraProvider(givenChainId?: SupportedChainID) {
+  // NOTE Passing the chainIds from useWallet doesn't work in the case of
+  // arbitrum and arbitrum-rinkeby. They need to be passed as objects.
+  // However, I have no idea why this is necessary. Looking at the ethers
+  // library, there's no reason why passing the chainId wouldn't work. Also,
+  // I've tried it on a fresh project and had no problems there...
+  // [VR 07-03-2022]
+  if (givenChainId === 42161) {
+    return new InfuraProvider(NW_ARB, INFURA_PROJECT_ID_ARB);
+  } else if (givenChainId === 421611) {
+    return new InfuraProvider(NW_ARB_RINKEBY, INFURA_PROJECT_ID_ARB);
+  } else {
+    return new InfuraProvider(givenChainId, INFURA_PROJECT_ID_ARB);
+  }
+}
+
+/**
+ * Returns provider based on the given chain id
+ * @param chainId network chain is
+ * @returns infura provider
+ */
+export function useSpecificProvider(chainId: SupportedChainID): InfuraProvider {
+  const [infuraProvider, setInfuraProvider] = useState(
+    getInfuraProvider(chainId)
+  );
+
+  useEffect(() => {
+    setInfuraProvider(getInfuraProvider(chainId));
+  }, [chainId]);
+
+  return infuraProvider;
 }
 
 /* CONTEXT CONSUMER ========================================================= */
