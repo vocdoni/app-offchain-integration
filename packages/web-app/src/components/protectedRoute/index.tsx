8a42b084464105b7676884b232614097614bd51f
Fabrice Francois <fabricevladimir@gmail.com>
2022-11-07 05:13:21 -0500

    comment out gating (#541)

--- packages/web-app/src/components/protectedRoute/index.tsx
+++ packages/web-app/src/components/protectedRoute/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, {useEffect} from 'react';
 import {Outlet} from 'react-router-dom';
 
@@ -11,56 +12,56 @@ import {Loading} from 'components/temporary';
 import {GatingMenu} from 'containers/gatingMenu';
 
 const ProtectedRoute: React.FC = () => {
-  const {data: dao, isLoading: paramIsLoading} = useDaoParam();
-  const {address, isConnected, status, isOnWrongNetwork} = useWallet();
-  const {data: daoDetails, isLoading: detailsAreLoading} = useDaoDetails(
-    dao || ''
-  );
-  const {open, close} = useGlobalModalContext();
-  const {
-    data: {filteredMembers},
-    isLoading: MembershipIsLoading,
-  } = useDaoMembers(
-    daoDetails?.plugins[0].instanceAddress || '',
-    daoDetails?.plugins[0].id as PluginTypes,
-    address as string
-  );
+  // const {data: dao, isLoading: paramIsLoading} = useDaoParam();
+  // const {address, isConnected, status, isOnWrongNetwork} = useWallet();
+  // const {data: daoDetails, isLoading: detailsAreLoading} = useDaoDetails(
+  //   dao || ''
+  // );
+  // const {open, close} = useGlobalModalContext();
+  // const {
+  //   data: {filteredMembers},
+  //   isLoading: MembershipIsLoading,
+  // } = useDaoMembers(
+  //   daoDetails?.plugins[0].instanceAddress || '',
+  //   daoDetails?.plugins[0].id as PluginTypes,
+  //   address as string
+  // );
 
-  useEffect(() => {
-    // Note: if user came to protected routes by direct link the status could be disconnected > connecting > connected
-    // In this scenario "close" on else case will help to fix unexpected behaviors at the wallet loading moment
-    if (!isConnected && status !== 'connecting') open('wallet');
-    else {
-      close('wallet');
-      if (isOnWrongNetwork) open('network');
-      else close('network');
-    }
+  // useEffect(() => {
+  //   // Note: if user came to protected routes by direct link the status could be disconnected > connecting > connected
+  //   // In this scenario "close" on else case will help to fix unexpected behaviors at the wallet loading moment
+  //   if (!isConnected && status !== 'connecting') open('wallet');
+  //   else {
+  //     close('wallet');
+  //     if (isOnWrongNetwork) open('network');
+  //     else close('network');
+  //   }
 
-    if (
-      filteredMembers.length === 0 &&
-      daoDetails &&
-      isConnected &&
-      !isOnWrongNetwork
-    ) {
-      open('gating');
-    }
-  }, [
-    close,
-    daoDetails,
-    filteredMembers.length,
-    isConnected,
-    isOnWrongNetwork,
-    open,
-    status,
-  ]);
+  //   if (
+  //     filteredMembers.length === 0 &&
+  //     daoDetails &&
+  //     isConnected &&
+  //     !isOnWrongNetwork
+  //   ) {
+  //     open('gating');
+  //   }
+  // }, [
+  //   close,
+  //   daoDetails,
+  //   filteredMembers.length,
+  //   isConnected,
+  //   isOnWrongNetwork,
+  //   open,
+  //   status,
+  // ]);
 
-  if (paramIsLoading || detailsAreLoading || MembershipIsLoading)
-    return <Loading />;
+  // if (paramIsLoading || detailsAreLoading || MembershipIsLoading)
+  //   return <Loading />;
 
   return (
     <>
       <Outlet />
-      <GatingMenu pluginType={daoDetails?.plugins[0].id as PluginTypes} />
+      {/* <GatingMenu pluginType={daoDetails?.plugins[0].id as PluginTypes} /> */}
     </>
   );
 };
