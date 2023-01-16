c64ed713f3c194ec44bcedadf83c32bdf9be395b
jamesej <jamesej@users.noreply.github.com>
2023-01-16 11:53:04 +0000

    Change subgraph url (#632)
    * Change subgraph url

--- packages/web-app/src/utils/constants/api.ts
+++ packages/web-app/src/utils/constants/api.ts
@@ -5,7 +5,7 @@ type SubgraphNetworkUrl = Record<SupportedNetworks, string | undefined>;
 export const SUBGRAPH_API_URL: SubgraphNetworkUrl = {
   ethereum: undefined,
   goerli:
-    'https://api.thegraph.com/subgraphs/name/aragon/aragon-zaragoza-goerli',
+    'https://subgraph.satsuma-prod.com/qHR2wGfc5RLi6/aragon/core-goerli/api',
   polygon: undefined,
   mumbai:
     'https://api.thegraph.com/subgraphs/name/aragon/aragon-zaragoza-mumbai',
