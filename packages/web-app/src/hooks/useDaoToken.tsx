707eb490128874dc1831d9c739c23d95edf2a1ae
Fabrice Francois <fabricevladimir@gmail.com>
2023-07-10 05:44:44 -0400

    Chore: APP-2308 - Bump SDK to v 1.9.5 (#912)
    * update SDK to 1.9.5
    
    * remove workaround for decimals and typing for DAO token

--- packages/web-app/src/hooks/useDaoToken.tsx
+++ packages/web-app/src/hooks/useDaoToken.tsx
@@ -1,9 +1,5 @@
+import {Erc20TokenDetails, Erc20WrapperTokenDetails} from '@aragon/sdk-client';
 import {useEffect, useState} from 'react';
-import {
-  TokenVotingClient,
-  Erc20TokenDetails,
-  Erc20WrapperTokenDetails,
-} from '@aragon/sdk-client';
 
 import {HookData} from 'utils/types';
 import {usePluginClient} from './usePluginClient';
@@ -17,9 +13,7 @@ export function useDaoToken(
   const [error, setError] = useState<Error>();
   const [isLoading, setIsLoading] = useState(false);
 
-  const pluginClient = usePluginClient(
-    'token-voting.plugin.dao.eth'
-  ) as TokenVotingClient;
+  const pluginClient = usePluginClient('token-voting.plugin.dao.eth');
 
   useEffect(() => {
     async function getDaoMetadata() {
@@ -27,22 +21,12 @@ export function useDaoToken(
         setIsLoading(true);
 
         const response = await pluginClient?.methods.getToken(pluginAddress);
+
         if (response) {
-          const underlyingToken = (
-            response as Erc20WrapperTokenDetails | undefined
-          )?.underlyingToken;
-
-          const finalResponse = response as Erc20TokenDetails;
-
-          setData({
-            ...finalResponse,
-            decimals: underlyingToken
-              ? underlyingToken.decimals
-              : finalResponse.decimals,
-          });
+          setData(response as Erc20TokenDetails | Erc20WrapperTokenDetails);
         }
       } catch (err) {
-        console.error(err);
+        console.error('Error fetching DAO token', err);
         setError(err as Error);
       } finally {
         setIsLoading(false);
