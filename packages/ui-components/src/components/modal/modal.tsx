a7d1179283437dd3ff0dce81737e47d78886f52f
Nikita Golubev <114730054+babarella@users.noreply.github.com>
2023-07-13 08:35:22 -0300

    fix: APP-2253 removed scroll from modals and bottom sheet components (#917)
    * fix: APP-2253 removed scroll from modals and bottom sheet components
    
    * refactor: applied radix overlay to modal and tweaked backdrop scroll hiding

--- packages/ui-components/src/components/modal/modal.tsx
+++ packages/ui-components/src/components/modal/modal.tsx
@@ -1,7 +1,14 @@
 import React, {ReactNode, CSSProperties} from 'react';
 import styled from 'styled-components';
-import {Root, Title, Content, Close, Portal} from '@radix-ui/react-dialog';
-import {Backdrop} from '../backdrop';
+import {
+  Root,
+  Title,
+  Content,
+  Close,
+  Portal,
+  Overlay,
+} from '@radix-ui/react-dialog';
+import {BackdropStyles} from '../backdrop';
 import {IconClose} from '../icons';
 
 export interface ModalProps {
@@ -52,7 +59,7 @@ export const Modal: React.FC<ModalProps> = ({
     <>
       <Root open={isOpen}>
         <Portal>
-          <Backdrop visible={isOpen} />
+          <ModalOverlay />
           <ModalContainer
             data-testid="modal-content"
             onInteractOutside={onInteractOutside}
@@ -124,4 +131,9 @@ const ModalSubtitle = styled.div.attrs({
 const ModalClose = styled(Close).attrs({
   className:
     'flex-shrink-0 text-ui-500 w-4 h-4 rounded-lg bg-ui-50 outline:none',
+})``;
+
+const ModalOverlay = styled(Overlay).attrs(() => {
+  const {className, css} = BackdropStyles({visible: true});
+  return {className, style: css};
 })``;
