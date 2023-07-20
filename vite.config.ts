52a3bb263eb5ab71745e364a8579da44cf261983
Ruggero <ruggero.cino@gmail.com>
2023-07-20 08:49:12 +0200

    fix: Reduce bundle size (#930)

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import {defineConfig, loadEnv} from 'vite';
 import {resolve} from 'path';
 import analyze from 'rollup-plugin-analyzer';
-//import {uglify} from 'rollup-plugin-uglify';
-
-//const production = process.env.NODE_ENV === 'production';
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
@@ -56,7 +53,6 @@ export default defineConfig(({mode}) => {
             stdout: true,
             summaryOnly: true,
           }),
-          //production && uglify(),
         ],
         output: {
           manualChunks: {
