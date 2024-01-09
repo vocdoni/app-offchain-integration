248c48f0c6aa64cb0fa46cedd64a74398794db20
Barukimang <barukimang@gmail.com>
2024-01-09 19:13:44 +0300

    fix: APP-2519 - Background overlays 'All Links' Modal (#1191)
    * added z-index so dropdown appears on top of illustrations
    
    * re-adding as string
    
    * add z-index through tailwind css class instead

--- src/@aragon/ods-old/components/dropdown/dropdown.tsx
+++ src/@aragon/ods-old/components/dropdown/dropdown.tsx
@@ -82,7 +82,7 @@ export const Dropdown: React.FC<DropdownProps> = ({
 };
 
 const StyledContent = styled(DropdownMenu.Content).attrs({
-  className: 'bg-neutral-0 rounded-xl p-2 shadow-neutral-xl' as string,
+  className: 'bg-neutral-0 rounded-xl p-2 shadow-neutral-xl z-50' as string,
 })``;
 
 const StyledItem = styled(DropdownMenu.Item).attrs({
